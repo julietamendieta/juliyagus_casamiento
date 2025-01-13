edfd4072cd8ae0ed86a032f26974cf805b976c14
Julieta Mendieta <julietamendieta1364@gmail.com>
2025-01-12 21:06:49 -0300

    invitación}

---
+++ script.js
@@ -0,0 +1,25 @@
+const daysLeft = document.querySelector(".countdown");
+const faltan = document.querySelector(".faltan");
+const dias = document.querySelector(".dias");
+const message = document.querySelector(".message")
+
+const countdownDate = new Date('09/27/2025').getTime();
+
+let interval = setInterval(() => {
+    const now = new Date().getTime();
+    let difference = countdownDate - now;
+    let days = Math.ceil(difference / (1000 * 60 * 60 * 24));
+
+    if(difference < 0){
+        clearInterval(interval);
+        message.innerHTML = "¡La fiesta es hoy!"
+    } else if(difference > 0 & days > 0 & days < 2){
+        faltan.innerHTML = "Falta"
+        daysLeft.innerHTML = days;
+        dias.innerHTML = "día"
+    } else{
+        faltan.innerHTML = "Faltan"
+        daysLeft.innerHTML = days
+        dias.innerHTML = "días"
+    }
+},1000);
